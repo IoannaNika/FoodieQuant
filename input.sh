0c614ca6df13e3ac5971fec4e70669d7d8a8e1dd
Ioanna Nika <ioanna@IoannassmallMBP.lan>
2023-06-11 15:02:18 +0200

    pixel counting & percentage estimation

--- input.sh
+++ input.sh
@@ -1,4 +1,4 @@
 #!/bin/bash          
-path="/Users/ioanna/Downloads/FoodSeg103/Images/img_dir/train/00000000.jpg"
+path="ice_cream.jpeg"
 output="output"
 python3 scripts/amg.py --checkpoint sam_vit_h_4b8939.pth --model-type vit_h --input $path --output $output --device cpu 
